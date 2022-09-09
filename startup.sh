a65924acb307f2e3f2f4db2af7c63014160f6060
Roger Jaffe <rogerjaffe@gmail.com>
2022-09-09 14:02:56 -0700

    Update startup script

--- startup.sh
+++ startup.sh
@@ -1,20 +1,39 @@
 #!/bin/sh
 
 # Set high resolution
-xrandr --newmode "1600x900"  118.25  1600 1696 1856 2112  900 903 908 934 -hsync +vsync
-xrandr --addmode eDP-1 "1600x900"
-xrandr -s "1600x900"
+#xrandr --newmode "1600x900"  118.25  1600 1696 1856 2112  900 903 908 934 -hsync +vsync
+#xrandr --addmode eDP-1 "1600x900"
+#xrandr -s "1600x900"
 
 # Reset wallpaper
 gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/mrj-wallpaper.jpg
 
-# Erase user files
-rm -rf /home/student
+# Move folders / files to retain
+mv /home/student/Desktop /home/temp
+mv /home/student/Documents /home/temp
+mv /home/student/Downloads /home/temp
+mv /home/student/NetBeansProjects /home/temp
+mv /home/student/Public /home/temp
+mv /home/student/snap /home/temp
+mv /home/student/Templates /home/temp
+mv /home/student/Videos /home/temp
+mv /home/student/VirtualBox\ VMs /home/temp
+mv /home/student/setup.sh /home/temp
 
-# Copy new home configuration
-cp -R /home/saved /home/student
-chown -R student student
-chgrp -R student student
+# Erase all other files in home folder
+rm -rf /home/student/*
+
+# Restore default folders
+mv /home/temp/* /home/student
+
+# Erase user files inside default folders
+rm -rf /home/student/Desktop/*
+rm -rf /home/student/Downloads/*
+rm -rf /home/student/Documents/*
+rm -rf /home/student/Public/*
+rm -rf /home/student/Templates/*
+rm -rf /home/student/Videos/*
+rm -rf /home/student/NetBeansProjects/*
 
 # Empty the trash using the trash-cli package
 trash-empty
